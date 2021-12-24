343c134a2d25a4b79b0d0e5b51c71b69f7e8b778
Jose Eduardo Chavez Franco <jechavezf@concredito.com.mx>
2021-12-24 14:41:09 -0700

    Canmbios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import logo from './assets/svg/rayo.svg'
 import { AppRouter } from './router/router';
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <AppRouter />
+//     </div>
+
+//   );
+// }
 
 function App() {
   return (
     <div className="app">
-      <AppRouter />
+      <header className="app-header">
+        <img src={logo} className="App-logo"></img>
+        <h1>jeckefe</h1>
+
+      </header>
+      <main className="main">
+        <div className="left__div">
+          En construcción
+        </div>
+        <div className="rigth__div"></div>
+      </main>
+      <footer className="footer__section">
+        <a className="footer__mail-link" href="mailto:eduardo@jeckefe.dev">Contacto</a>
+      </footer>
     </div>
 
   );
