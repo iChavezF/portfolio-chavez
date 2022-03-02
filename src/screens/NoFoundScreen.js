2df2330ee0b397c3515d7c9d2ddd9c8ccde20dcc
Jose Eduardo Chavez Franco <jechavezf@concredito.com.mx>
2022-03-02 15:33:10 -0700

    Se aniade bug responsive en mediaquery

--- src/screens/NoFoundScreen.js
+++ src/screens/NoFoundScreen.js
@@ -5,7 +5,7 @@ export const NotFoundScreen = () => {
     return (
         <div>
             <HeadManager title='not found' />
-            Not Found
+            <img  className = 'about-wrapper' src='https://http.cat/404' alt='notfound'></img>
         </div>
     )
 }
